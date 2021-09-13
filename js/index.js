44f90c1151c641b8b7f454158fbddcc65479abba
KatIsCoding <fabrigoru@gmail.com>
2021-09-13 12:46:24 -0600

    Linters and some Bugs in localStorage

--- js/index.js
+++ js/index.js
@@ -1,70 +1,61 @@
-var books = [];
+let books = [];
 
 function searchID(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
-
-
-
 
 function showBook(object) {
-    var name = object["bookTitle"]
-    var author = object["bookAuthor"]
+  const name = object.bookTitle;
+  const author = object.bookAuthor;
 
-    var booksWrapper = searchID("booksSection")
+  const booksWrapper = searchID('booksSection');
 
-    var container = document.createElement("div")
-    container.classList.add("bookContainer")
-    container.id = name
+  const container = document.createElement('div');
+  container.classList.add('bookContainer');
+  container.id = name;
 
-    var bookName = document.createElement("h3")
-    bookName.innerText = name
-    container.appendChild(bookName)
+  const bookName = document.createElement('h3');
+  bookName.innerText = name;
+  container.appendChild(bookName);
 
-    var bookAuthor = document.createElement("h4")
-    bookAuthor.innerText = author
-    container.appendChild(bookAuthor)
+  const bookAuthor = document.createElement('h4');
+  bookAuthor.innerText = author;
+  container.appendChild(bookAuthor);
 
-    var deleteButton = document.createElement("button")
-    deleteButton.addEventListener("click", () => {
-        container.remove();
-        var filtered = books.filter( (element) => {
-            return element["bookTitle"] !== name;
-        });
-        books = filtered;
-        localStorage["booksObjects"] = JSON.stringify(books)
-    })
-    deleteButton.innerText = "Delete"
-    container.appendChild(deleteButton)
+  const deleteButton = document.createElement('button');
+  deleteButton.addEventListener('click', () => {
+    container.remove();
+    const filtered = books.filter((element) => element.bookTitle !== name);
+    books = filtered;
+    localStorage.booksObjects = JSON.stringify(books);
+  });
+  deleteButton.innerText = 'Delete';
+  container.appendChild(deleteButton);
 
-    booksWrapper.appendChild(container)
+  booksWrapper.appendChild(container);
 }
-
-
 
 function addBooks() {
-    var title = searchID("bookName").value
-    var author = searchID("bookAuthor").value
-    let object = {
-        bookTitle: title,
-        bookAuthor: author
-    }
-    books.push(object);
-    showBook(object);
-    localStorage["booksObjects"] = JSON.stringify(books)
+  const title = searchID('bookName').value;
+  const author = searchID('bookAuthor').value;
+  const object = {
+    bookTitle: title,
+    bookAuthor: author,
+  };
+  books.push(object);
+  showBook(object);
+  localStorage.booksObjects = JSON.stringify(books);
 }
-
-
-
 
 window.onload = () => {
-    var btn = searchID("addBookButton")
-    btn.addEventListener("click", () => {
-        addBooks()
-    })
-    if (localStorage["booksObjects"] !== undefined){
-        JSON.parse(localStorage["booksObjects"]).forEach(element => {
-            showBook(element)
-        });
-    }
-}
+  const btn = searchID('addBookButton');
+  btn.addEventListener('click', () => {
+    addBooks();
+  });
+  if (localStorage.booksObjects !== undefined) {
+    books = JSON.parse(localStorage.booksObjects)
+    JSON.parse(localStorage.booksObjects).forEach((element) => {
+      showBook(element);
+    });
+  }
+};
