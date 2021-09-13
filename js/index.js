36209b4ef2e6b04d99e19a6c125f9683f210768f
KatIsCoding <fabrigoru@gmail.com>
2021-09-13 11:47:01 -0600

    Add new book functionality

--- js/index.js
+++ js/index.js
@@ -1,19 +1,59 @@
 var books = [];
 
-function addBooks(title, author) {
-    
+function searchID(id) {
+    return document.getElementById(id);
+}
+
+
+
+
+function showBook(object) {
+    var name = object["bookTitle"]
+    var author = object["bookAuthor"]
+
+    var booksWrapper = searchID("booksSection")
+
+    var container = document.createElement("div")
+    container.classList.add("bookContainer")
+    container.id = name
+
+    var bookName = document.createElement("h3")
+    bookName.innerText = name
+    container.appendChild(bookName)
+
+    var bookAuthor = document.createElement("h4")
+    bookAuthor.innerText = author
+    container.appendChild(bookAuthor)
+
+    var deleteButton = document.createElement("button")
+    deleteButton.addEventListener("click", () => {
+        console.log(name)
+    })
+    deleteButton.innerText = "Delete"
+    container.appendChild(deleteButton)
+
+    booksWrapper.appendChild(container)
+}
+
+
+
+function addBooks() {
+    var title = searchID("bookName").value
+    var author = searchID("bookAuthor").value
     let object = {
         bookTitle: title,
         bookAuthor: author
     }
-
     books.push(object);
+    showBook(object);
 }
 
-function deleteBooks() {
 
-    var bookToDelete = document.getElementById('bookSection');
-    var children = bookToDelete.children;
-    children.forEach( this.filter());
 
+
+window.onload = () => {
+    var btn = searchID("addBookButton")
+    btn.addEventListener("click", () => {
+        addBooks()
+    })
 }
